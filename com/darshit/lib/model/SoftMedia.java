459bc9a32850d2f26603185f57894679a68afc10
Darshitrawal <45291509+Darshitrawal@users.noreply.github.com>
2019-09-16 18:43:39 +0530

    Delete SoftMedia.java

--- com/darshit/lib/model/SoftMedia.java
+++
@@ -1,9 +0,0 @@
-package com.darshit.lib.model;
-
-public class SoftMedia extends Media{
-  public String size;
-  SoftMedia(int id, String title, String size, String location, String authorName){
-    super(title, id, location, authorName);
-    this.size = size;
-  }
-}
